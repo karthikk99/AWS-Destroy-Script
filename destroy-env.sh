7a967af005a07eba9a2ea00ff556b50e21234b80
Karthik Keshavamurthy <kkeshava@hawk.iit.edu>
2016-11-01 09:23:17 -0500

    Update destroy-env.sh

--- destroy-env.sh
+++ destroy-env.sh
@@ -7,45 +7,49 @@ echo ""
 echo "Collecting all the 4 instances in running status........"
 instances=$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].[Placement.AvailabilityZone, State.Name, InstanceId]' --output text | grep us-west-2b | grep 'running' | awk '{print $3}')
 
+autoscalinggroup=$(aws autoscaling describe-auto-scaling-instances --query '')
+
 # detach all the instances from the auto scaling group
 echo ""
-echo "Detatching all the 4 instances from WEBSERVERDEMO auto-scaling-group"
-aws autoscaling detach-instances --instance-ids $instances --auto-scaling-group-name webserverdemo --should-decrement-desired-capacity
+echo "Detatching all the 4 instances from auto-scaling-group"
+aws autoscaling detach-instances --instance-ids $instances --auto-scaling-group-name $autoscalinggroup --should-decrement-desired-capacity
 
 # aws autoscaling delete autoscaling configuration
 echo ""
-echo "Deleting auto-scaling-group WEBSERVERDEMO............"
-aws autoscaling delete-auto-scaling-group --auto-scaling-group-name webserverdemo --force-delete
+echo "Deleting auto-scaling-group............"
+aws autoscaling delete-auto-scaling-group --auto-scaling-group-name $autoscalinggroup --force-delete
 echo ""
-echo "WEBSERVERDEMO deleted successfully!"
+echo "Auto-scaling group deleted successfully!"
 
+launchconfig=$(aws autoscaling describe-launch-configurations --query '')
 # aws autoscaling delete-launch configuration
 echo ""
-echo "Deleting launch configuration WEBSERVER..............."
-aws autoscaling delete-launch-configuration --launch-configuration-name webserver
+echo "Deleting launch configuration..............."
+aws autoscaling delete-launch-configuration --launch-configuration-name $launchconfig
 echo ""
-echo "WEBSERVER deleted successfully!"
+echo "Launch Configuration deleted successfully!"
 
+loadbalancer=$(aws elb describe-load-balancers --query '')
 #deregister instances
 echo ""
 echo "Deregistering the instances from the load-balancer MY-LOAD-BALANCER......"
-aws elb deregister-instances-from-load-balancer --load-balancer-name my-load-balancer --instances $instances
+aws elb deregister-instances-from-load-balancer --load-balancer-name $loadbalancer --instances $instances
 echo ""
 echo "Deregistered instances successfully!"
 
 # aws elb delete listeners 
 echo ""
 echo "Deleting the listener created for MY-LOAD-BALANCER......"
-aws elb delete-load-balancer-listeners --load-balancer-name my-load-balancer --load-balancer-ports 80
+aws elb delete-load-balancer-listeners --load-balancer-name $loadbalancer --load-balancer-ports 80
 echo ""
 echo "Listener deleted successfully!"
 
 # aws elb delete load-balancers
 echo ""
 echo "Deleing the load-balancer MY-LOAD-BALANCER............."
-aws elb delete-load-balancer --load-balancer-name my-load-balancer
+aws elb delete-load-balancer --load-balancer-name $loadbalancer
 echo ""
-echo "MY-LOAD-BALANCER deleted successfully!"
+echo "LOAD-BALANCER deleted successfully!"
 
 # delete all the instances
 echo ""
